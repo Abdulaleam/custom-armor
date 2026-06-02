61cf8f6500e909fdce9a9a83a779a58600168a5b
Abdulaleam <167490133+Abdulaleam@users.noreply.github.com>
2026-06-02 13:45:18 +0300

    Initial commit

---
+++ src/main/java/net/rainy/armor/CustomArmorDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rainy.armor;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CustomArmorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
