3da10152779a5862b2637227fc30f1e672eeaaf7
Abdulaleam <167490133+Abdulaleam@users.noreply.github.com>
2026-06-04 08:11:27 +0300

    FIXED THE FROSTY EFFECT , i had the block set to ice not packed ice

--- src/main/java/net/rainy/armor/effect/FrostyEffect.java
+++ src/main/java/net/rainy/armor/effect/FrostyEffect.java
@@ -8,9 +8,9 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class FrostyEffect extends StatusEffect {
+
     public FrostyEffect(StatusEffectCategory category, int color) {
         super(category, color);}
-
     @Override
     public boolean applyUpdateEffect(LivingEntity entity, int amplifier) {
         if (entity.isSneaking())
@@ -20,8 +20,9 @@ public class FrostyEffect extends StatusEffect {
         {return true;}
         if (entity.getVelocity().horizontalLengthSquared() > 0.05) {
             BlockPos pos = entity.getBlockPos().down();
-            world.setBlockState(pos, Blocks.ICE.getDefaultState());}
-        return true;}
+            world.setBlockState(pos, Blocks.PACKED_ICE.getDefaultState());}
+        return true;
+    }
 
     @Override
     public boolean canApplyUpdateEffect(int duration, int amplifier) {
